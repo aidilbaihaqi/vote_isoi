6683483cff7d11d1af33dd1e8bd0c642e210e91c
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-12-10 17:29:05 +0700

    test favicon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Voting Ketua Umum ISOI</title>
-    <link rel="shortcut icon" type="image/x-icon href="{{ asset('favicon.ico') }}?v=2" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
